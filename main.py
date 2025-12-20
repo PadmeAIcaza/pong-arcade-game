5558f1774e85a17a18b7fa30e9d400b065d50ff2
spicypusheen <padme.garrapata@gmail.com>
2025-12-20 10:18:56 -0600

    Ball bouncing logic

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ while game_on:
     screen.update()
     ball.move()
 
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
 
 screen.exitonclick()
 
